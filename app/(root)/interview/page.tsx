ea66e58f1b8b417cc82efd88a4859b9b3ff036a4
Dimple Chauhan <dimplechauhan312@gmail.com>
2026-02-06 13:05:51 +0530

    remove auth dependency from interview generate page

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,17 +1,21 @@
+
 import Agent from "@/components/Agent";
-import { getCurrentUser } from "@/lib/actions/auth.action";
 
 const Page = async () => {
-    const user = await getCurrentUser();
+    // 🔕 Auth temporarily removed for deployment
+    const user = {
+        name: "Guest User",
+        id: "guest-id",
+        profileURL: null,
+    };
 
     return (
         <>
             <h3>Interview generation</h3>
 
             <Agent
-                userName={user?.name!}
-                userId={user?.id}
-                profileImage={user?.profileURL}
+                userName={user.name}
+                userId={user.id}
                 type="generate"
             />
         </>
@@ -19,3 +23,4 @@ const Page = async () => {
 };
 
 export default Page;
+
